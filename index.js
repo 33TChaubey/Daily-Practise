394a12baf3d502c60ba2bc362b5c233c28e06797
Trilokinath Chaubey <chaubeytn2103@outlook.com>
2024-03-31 01:04:15 +0530

    Printing the window of Browssr Page

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var date = new Date();
-console.log('Today is: ', date.toLocaleString("eng-US", {weekday:'long'}));
-console.log('Current time is :', date.toLocaleString("en-US", {timeZone:'IST', hour:'numeric', hour12: true}) + '' + date.getHours() + ':' + date.getMinutes());
+// var date = new Date();
+// console.log('Today is: ', date.toLocaleString("eng-US", {weekday:'long'}));
+// console.log('Current time is :', date.toLocaleString("en-US", {timeZone:'IST', hour:'numeric', hour12: true}) + '' + date.getHours() + ':' + date.getMinutes());
+
+
+function print_current_page(){
+    window.print()
+}
