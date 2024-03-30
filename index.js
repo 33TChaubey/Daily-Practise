cb0154fede302872ad86130b9ffd66e41bf9d2fe
Trilokinath Chaubey <chaubeytn2103@outlook.com>
2024-03-31 00:59:21 +0530

    Getting Time Via JS

---
+++ index.js
@@ -0,0 +1,3 @@
+var date = new Date();
+console.log('Today is: ', date.toLocaleString("eng-US", {weekday:'long'}));
+console.log('Current time is :', date.toLocaleString("en-US", {timeZone:'IST', hour:'numeric', hour12: true}) + '' + date.getHours() + ':' + date.getMinutes());
