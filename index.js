219980fcc3070cb444ab8534599cec1aa167d8df
Trilokinath Chaubey <chaubeytn2103@outlook.com>
2024-03-31 01:28:50 +0530

    JavaScript : Display the current date

--- index.js
+++ index.js
@@ -3,6 +3,27 @@
 // console.log('Current time is :', date.toLocaleString("en-US", {timeZone:'IST', hour:'numeric', hour12: true}) + '' + date.getHours() + ':' + date.getMinutes());
 
 
-function print_current_page(){
-    window.print()
+
+//JavaScript: Print the contents of the current window
+
+// function print_current_page(){
+//     window.print()
+//}
+
+
+// JavaScript : Display the current date
+let date = new Date();
+let dd= date.getDate();
+let mm = date.getMonth()+1;
+let yyyy=date.getFullYear();
+
+if (dd < 10){
+    dd=`0${dd}`
 }
+
+if (mm < 10){
+    mm=`0${mm}`
+}
+
+today = `${dd}-${mm}-${yyyy}`;
+console.log(today);
