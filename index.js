f6b7dbea7e2150330fcec907efc8446972e007f0
Trilokinath Chaubey <chaubeytn2103@outlook.com>
2024-04-01 11:42:34 +0530

    js to ts

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-// var date = new Date();
-// console.log('Today is: ', date.toLocaleString("eng-US", {weekday:'long'}));
-// console.log('Current time is :', date.toLocaleString("en-US", {timeZone:'IST', hour:'numeric', hour12: true}) + '' + date.getHours() + ':' + date.getMinutes());
-
-
-
-//JavaScript: Print the contents of the current window
-
-// function print_current_page(){
-//     window.print()
-//}
-
-
-// JavaScript : Display the current date
-// let date = new Date();
-// let dd= date.getDate();
-// let mm = date.getMonth()+1;
-// let yyyy=date.getFullYear();
-
-// if (dd < 10){
-//     dd=`0${dd}`
-// }
-
-// if (mm < 10){
-//     mm=`0${mm}`
-// }
-
-// today = `${dd}-${mm}-${yyyy}`;
-// console.log(today);
+//console.log("Hall of fame");
+var date = new Date();
+var dd = date.getDate();
+var mm = date.getMonth() + 1;
+var yyyy = date.getFullYear();
+if (dd < 10) {
+    dd = "0".concat(dd);
+}
+if (mm < 10) {
+    mm = "0".concat(mm);
+}
+var today = "".concat(dd, "-").concat(mm, "-").concat(yyyy);
+console.log(today);
